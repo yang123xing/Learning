ada2bdaec8cf6a85a6cbe1dec9572ca22f3577aa
Administrator <Administrator@k-0144>
2017-11-09 09:44:44 +0800

    fdsasf

---
+++ src/main/java/com/yangxing/demo/Demo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.yangxing.demo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello word");
+	}
+
+}
